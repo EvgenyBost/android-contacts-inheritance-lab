6718c81592dde529e49b5709a8b2dc423331e08b
Evgeny Bostynets <jhenekb@gmail.com>
2023-10-21 23:40:20 +0300

    Code refactoring. Create parent class for two same classes

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeUI.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeUI.java
@@ -2,42 +2,27 @@ package ru.yandex.practicum.contacts.presentation.sort;
 
 import androidx.annotation.NonNull;
 
-public class SortTypeUI {
+import ru.yandex.practicum.contacts.presentation.base.ContactsOrderTypeUi;
 
-    private final String type;
-    private final boolean isSelected;
+public class SortTypeUI extends ContactsOrderTypeUi {
 
-    public SortTypeUI(@NonNull String type, boolean isSelected) {
-        this.type = type;
-        this.isSelected = isSelected;
+    public SortTypeUI(@NonNull String type, boolean isSelected){
+        super(type, isSelected);
     }
 
-    public String getType() {
-        return type;
-    }
-
-    public boolean isSelected() {
-        return isSelected;
-    }
-
+    @Override
     public String createLogMessage() {
         return "Выбран тип сортировки: " + type;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
         SortTypeUI that = (SortTypeUI) o;
 
-        if (isSelected != that.isSelected) {
-            return false;
-        }
+        if (isSelected != that.isSelected) return false;
         return type.equals(that.type);
     }
 
@@ -47,4 +32,5 @@ public class SortTypeUI {
         result = 31 * result + (isSelected ? 1 : 0);
         return result;
     }
+
 }
